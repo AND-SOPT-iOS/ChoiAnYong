05d7d05c382d47c420609153f9784d714ebdb68d
ChoiAnYong <dksdyd78@naver.com>
2024-10-08 23:20:08 +0900

    feat: 프로젝트 세팅

---
+++ 35-Seminar/35-Seminar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  35-Seminar
+//
+//  Created by 최안용 on 10/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
