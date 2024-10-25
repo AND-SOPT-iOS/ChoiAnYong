a0b20697bc7f88643cec2c35f44770bc0568b4a8
ChoiAnYong <dksdyd78@naver.com>
2024-10-25 22:42:04 +0900

    feat: 화면 전환 구현

---
+++ 35-Seminar-Week2/35-Seminar-Week2/Presentation/View/SeeAllReviewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SeeAllReviewViewController.swift
+//  35-Seminar-Week2
+//
+//  Created by 최안용 on 10/25/24.
+//
+
+import UIKit
+
+class SeeAllReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
