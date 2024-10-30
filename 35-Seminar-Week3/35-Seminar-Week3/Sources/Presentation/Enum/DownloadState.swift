ff6d6e88d0a69048905570a807263e41ef18cb75
ChoiAnYong <dksdyd78@naver.com>
2024-10-30 18:41:08 +0900

    feat: DownloadState title, image, formatting 추가

--- 35-Seminar-Week3/35-Seminar-Week3/Sources/Presentation/Enum/DownloadState.swift
+++ 35-Seminar-Week3/35-Seminar-Week3/Sources/Presentation/Enum/DownloadState.swift
@@ -6,10 +6,39 @@
 //
 
 import Foundation
+import UIKit
 
-enum DownloadState: String {
-    case installed = "열기"
-    case download = "받기"
-    case reDownload = "다시받기"
-    case update = "업데이트"
+enum DownloadState {
+    case downloaded
+    case download
+    case reDownload
+    case update
+    case cost(cost: Int)
+    
+    var title: String? {
+        switch self {
+        case .downloaded: return "열기"
+        case .download: return "받기"
+        case .reDownload: return nil
+        case .update: return "업데이트"
+        case .cost(let cost):
+            return numberFormatting(number: cost)
+        }
+    }
+    
+    var image: UIImage? {
+        if case .reDownload = self {
+            return UIImage(systemName: "icloud.and.arrow.down")
+        }
+        return nil
+    }
+    
+    private func numberFormatting(number: Int) -> String {
+        let value = number
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        let result = numberFormatter.string(for: value) ?? "0"
+        
+        return "₩"+result
+    }
 }
