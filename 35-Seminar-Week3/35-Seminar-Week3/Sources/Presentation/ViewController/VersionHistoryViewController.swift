25ff52b5d44ba381129c8f6ed6dcbf8a6f3a2e2f
ChoiAnYong <dksdyd78@naver.com>
2024-11-01 17:29:16 +0900

    feat: 3주차 과제 코드 추가(#5)

---
+++ 35-Seminar-Week3/35-Seminar-Week3/Sources/Presentation/ViewController/VersionHistoryViewController.swift
@@ -0,0 +1,17 @@
+//
+//  VersionHistoryViewController.swift
+//  35-Seminar-Week2
+//
+//  Created by 최안용 on 10/25/24.
+//
+
+import UIKit
+
+class VersionHistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+}
