7bae899bd655aea395cd45de7782967be1cc8205
ChoiAnYong <dksdyd78@naver.com>
2024-11-07 16:29:26 +0900

    feat: BaseViewController 구현

---
+++ 35-Seminar-Week4/35-Seminar-Week4/General/Base/BaseViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BaseViewController.swift
+//  35-Seminar-Week4
+//
+//  Created by 최안용 on 11/6/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setStyle()
+        setUI()
+        setLayout()
+    }
+    
+    func setStyle() {
+        
+    }
+    
+    func setUI() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+}
